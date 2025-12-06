52c12f83b080877c13420ba1a43c54dec7765679
Kiwi <chenyujie217@163.com>
2025-12-06 15:41:00 -0500

    secure false

--- api/index.js
+++ api/index.js
@@ -83,7 +83,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     sameSite: 'lax'
   }
 }));
